59314a797c3091b7e8abaa5544ef0422d9fa60ef
Nathan Spencer <natekspencer@gmail.com>
2021-08-09 17:43:01 -0600

    Add binary sensor for motion to cameras

--- custom_components/vivint/binary_sensor.py
+++ custom_components/vivint/binary_sensor.py
@@ -1,4 +1,6 @@
 """Support for Vivint binary sensors."""
+from datetime import datetime, timedelta
+
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_COLD,
     DEVICE_CLASS_DOOR,
@@ -12,11 +14,18 @@ from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_WINDOW,
     BinarySensorEntity,
 )
+from homeassistant.core import CALLBACK_TYPE, callback
+from homeassistant.helpers.event import async_call_later
+from homeassistant.util.dt import utcnow
+from vivintpy.devices import VivintDevice
+from vivintpy.devices.camera import MOTION_DETECTED, Camera
 from vivintpy.devices.wireless_sensor import WirelessSensor
 from vivintpy.enums import EquipmentType, SensorType
 
 from .const import DOMAIN
-from .hub import VivintEntity
+from .hub import VivintEntity, VivintHub
+
+MOTION_STOPPED_SECONDS = 30
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -29,6 +38,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             for device in alarm_panel.devices:
                 if type(device) is WirelessSensor:
                     entities.append(VivintBinarySensorEntity(device=device, hub=hub))
+                elif type(device) is Camera:
+                    entities.append(
+                        VivintCameraBinarySensorEntity(device=device, hub=hub)
+                    )
 
     if not entities:
         return
@@ -91,3 +104,71 @@ class VivintBinarySensorEntity(VivintEntity, BinarySensorEntity):
 
         else:
             return DEVICE_CLASS_SAFETY
+
+
+class VivintCameraBinarySensorEntity(VivintEntity, BinarySensorEntity):
+    """Vivint Camera Binary Sensor."""
+
+    def __init__(self, device: VivintDevice, hub: VivintHub):
+        """Pass coordinator to CoordinatorEntity."""
+        super().__init__(device=device, hub=hub)
+        self._last_motion_event: datetime = None
+        self._motion_stopped_callback: CALLBACK_TYPE = None
+
+    @property
+    def name(self):
+        """Return the name of this entity."""
+        return f"{self.device.name} Motion"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return f"{self.device.alarm_panel.id}-{self.device.id}"
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if the binary sensor is on."""
+        return (
+            self._last_motion_event
+            and self._last_motion_event >= utcnow() - timedelta(seconds=30)
+        )
+
+    @property
+    def device_class(self):
+        """Return the class of this device."""
+        return DEVICE_CLASS_MOTION
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        await super().async_added_to_hass()
+        self.device.on(MOTION_DETECTED, self._motion_callback)
+
+    async def async_will_remove_from_hass(self):
+        """Disconnect callbacks."""
+        await super().async_will_remove_from_hass()
+        self.async_cancel_motion_stopped_callback()
+
+    @callback
+    def _motion_callback(self, _):
+        """Call motion method."""
+        self.async_cancel_motion_stopped_callback()
+
+        self._last_motion_event = utcnow()
+        self.async_write_ha_state()
+
+        self._motion_stopped_callback = async_call_later(
+            self.hass, MOTION_STOPPED_SECONDS, self.async_motion_stopped_callback
+        )
+
+    async def async_motion_stopped_callback(self, *_) -> None:
+        """Motion stopped callback."""
+        self._motion_stopped_callback = None
+        self._last_motion_event = None
+        self.async_write_ha_state()
+
+    @callback
+    def async_cancel_motion_stopped_callback(self):
+        """Clear the motion stopped callback if it has not already fired."""
+        if self._motion_stopped_callback is not None:
+            self._motion_stopped_callback()
+            self._motion_stopped_callback = None
