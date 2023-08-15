8cc1043fa1e0b715a1bcb1842f92e1b46c667e93
Nathan Spencer <natekspencer@gmail.com>
2023-08-15 16:15:11 +0000

    Avoid requesting a refresh right away on setup

--- custom_components/vivint/alarm_control_panel.py
+++ custom_components/vivint/alarm_control_panel.py
@@ -61,7 +61,7 @@ async def async_setup_entry(
     if not entities:
         return
 
-    async_add_entities(entities, True)
+    async_add_entities(entities)
 
 
 class VivintAlarmControlPanelEntity(VivintEntity, AlarmControlPanelEntity):
